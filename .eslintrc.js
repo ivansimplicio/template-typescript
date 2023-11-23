2213768673b625f7c30ef49470c1483e40d1f11e
ivansimplicio <ivansimplicio@outlook.com>
2023-11-23 02:38:49 +0000

    chore: configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,33 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2022: true,
     node: true,
   },
-  extends: "standard-with-typescript",
+  extends: ['standard-with-typescript', 'prettier'],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  rules: {},
-};
+  plugins: ['prettier'],
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
